3f31b0564669169648e09e6ba1fb76cfd97299df
Jacob Jolibois <62900807+j0lib0is@users.noreply.github.com>
2023-02-07 22:04:29 +0000

    adding authentication with supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { supabase } from './supabaseClient';
+import Auth from './components/Auth';
+import Account from './pages/Account';
 
 // Import Pages
 import Home from './pages/Home';
@@ -14,19 +17,26 @@ import { SheetProvider } from './context/SheetContext';
 
 export default function App() {
 
-  // Helpers
-  // const postSheet = newSheet => {
-  //   axios.post('url', newSheet)
-  //     .then(res => {
-  //       console.log(res);
-  //       setSheetList([res.data, ...sheetList]);
-  //     })
-  //     .catch(err => console.error(err))
-  //     .finally(setSheetValues(defaultSheetValues))
-  // }
+  // State
+  const [session, setSession] = useState(null);
+
+  // Use Effect
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    });
+    console.log(session);
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    });
+  }, []);
 
   return (
     <div className='max-w-2xl mx-auto'>
+      <div className='container' style={{ padding: '50px 0 100px 0' }}>
+        {!session ? <Auth /> : <Home />}
+      </div>
       <SheetListProvider>
         <SheetProvider>
           <Router>
@@ -34,18 +44,14 @@ export default function App() {
               <Link className='mx-4 text-center' to='/'>Home</Link>
               <Link className='mx-4 text-center' to='/characters'>My Characters</Link>
               <Link className='mx-4 text-center' to='/new-character'>New Character</Link>
+              <Link className='mx-4 text-center' to='/account'>Account</Link>
             </nav>
             <Routes>
               <Route path='/' element={<Home />} />
-              <Route path='/characters' element={
-                <Characters />
-              } />
-              <Route path='/character/:characterId' element={
-                <CharacterSheet />
-              } />
-              <Route path='/new-character' element={
-                <NewCharacter />
-              } />
+              <Route path='/characters' element={<Characters />} />
+              <Route path='/character/:characterId' element={<CharacterSheet />} />
+              <Route path='/new-character' element={<NewCharacter />} />
+              {/*<Route path='/account' element={<Account key={session.user.id} session={session} />} />*/}
               <Route path='*' element={<Error />} />
             </Routes>
             <div className='text-center p-4'>
